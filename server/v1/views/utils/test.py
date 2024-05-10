4494a56a4544a8b8818d654225f2d002b1a68091
Aanuoluwa101 <aanuayodeji101@gmail.com>
2024-05-10 05:19:17 +0100

    hardcoded responses

--- server/v1/views/utils/test.py
+++ server/v1/views/utils/test.py
@@ -15,189 +15,188 @@
 # # print(''.join(output))
 
 
-# import requests
-
-# def test_chatgpt_api(api_key, prompt):
-#     url = "https://api.openai.com/v1/chat/completions"
-#     headers = {
-#         "Authorization": f"Bearer {api_key}",
-#         "Content-Type": "application/json"
-#     }
-#     data = {
-#         "model": "gpt-3.5-turbo-0125",
-#         "messages": [{"role": "system", "content": prompt}]
-#     }
-#     response = requests.post(url, json=data, headers=headers)
-    
-#     if response.status_code == 200:
-#         return response.json()["choices"][0]["message"]["content"]
-#     else:
-#         return f"Error: {response.status_code} - {response.text}"
-
-# # Replace 'YOUR_API_KEY' with your actual API key
-
-# # Sample prompt
-# prompt = "Hello, how are you?"
-
-# # Test the API
-# response = test_chatgpt_api(api_key, prompt)
-# print("Response from ChatGPT:", response)
-
-
-# from db import word_of_the_day
-# from datetime import datetime
-
-# current_date = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-
-#     # Find document with date matching current date
-# document = word_of_the_day.find_one({"date": current_date})
-
-# print(str(datetime.now().date()))
-
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-uri = os.getenv('DB_URI')
-#uri = "mongodb+srv://soundsophisticated:soundsophisticated123@mercymercy.dyidanr.mongodb.net/?retryWrites=true&w=majority&appName=MercyMercy"
-#uri = 'mongodb://soundsophisticated:soundsophisticated123@localhost:27017/soundsophisticated'
-
-
-data = [
-  {
-    "word": "pronunciation",
-    "phonetic": "/pɹəˌnaʊn.siˈeɪ.ʃən/",
-    "phonetics": [
-      {
-        "text": "/pɹəˌnaʊn.siˈeɪ.ʃən/",
-        "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/pronunciation-us.mp3",
-        "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=589419",
-        "license": {
-          "name": "BY-SA 3.0",
-          "url": "https://creativecommons.org/licenses/by-sa/3.0"
-        }
-      }
-    ],
-    "meanings": [
-      {
-        "partOfSpeech": "noun",
-        "definitions": [
-          {
-            "definition": "The formal or informal way in which a word is made to sound when spoken.",
-            "synonyms": [],
-            "antonyms": [],
-            "example": "What is the pronunciation of \"hiccough\"?"
-          },
-          {
-            "definition": "The way in which the words of a language are made to sound when speaking.",
-            "synonyms": [],
-            "antonyms": [],
-            "example": "His Italian pronunciation is terrible."
-          },
-          {
-            "definition": "The act of pronouncing or uttering something.",
-            "synonyms": [],
-            "antonyms": []
-          }
-        ],
-        "synonyms": [],
-        "antonyms": []
-      }
-    ],
-    "license": {
-      "name": "CC BY-SA 3.0",
-      "url": "https://creativecommons.org/licenses/by-sa/3.0"
-    },
-    "sourceUrls": [
-      "https://en.wiktionary.org/wiki/pronunciation"
-    ]
-  }
-]
-
 import requests
-import json
-from constants import SUGGEST_WORD_OF_THE_DAY_SYSTEM_ROLE, SUGGEST_EXAMPLE_SYSTEM_ROLE, SUGGEST_WORD_SYSTEM_ROLE, payload, headers
 
-def suggest_example(word_and_definition):
-        messages = [{"role": "system", "content": f"{SUGGEST_EXAMPLE_SYSTEM_ROLE}"}]
-        messages.append({"role": "user", "content": f"{word_and_definition}"})
-        payload["messages"] = messages
-
-        try:
-           response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-           if response.status_code == 200:
-              return json.loads(response.json()["choices"][0]['message']['content'])
-        except Exception as e:
-           #we'll log these erros later
-           print(e)
-
-
-def extract_definitions(data):
-    all_definitions = []
-    for meaning in data["meanings"]:
-        for definition in meaning["definitions"]:
-            lean_definition = {
-                "word": data["word"], 
-                "part_of_speech": meaning["partOfSpeech"],
-                "definition": definition["definition"],
-                "example": definition.get("example", None)
-            }
-            all_definitions.append(lean_definition)
-
-    return all_definitions
-
-
-definitions = [
-    {
-        'word': 'pronunciation',
-        'part_of_speech': 'noun',
-        'definition': 'The formal or informal way in which a word is made to sound when spoken.',
-        'example': 'What is the pronunciation of "hiccough"?'
-    },
-    {
-        'word': 'pronunciation',
-        'part_of_speech': 'noun',
-        'definition': 'The way in which the words of a language are made to sound when speaking.',
-        'example': None
-    },
-    {
-        'word': 'pronunciation',
-        'part_of_speech': 'noun',
-        'definition': 'The act of pronouncing or uttering something.',
-        'example': None
+def test_chatgpt_api(api_key, prompt):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
     }
-]
+    data = {
+        "model": "gpt-3.5-turbo-0125",
+        "messages": [{"role": "system", "content": prompt}]
+    }
+    response = requests.post(url, json=data, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        return f"Error: {response.status_code} - {response.text}"
+
+# Replace 'YOUR_API_KEY' with your actual API key
+
+# Sample prompt
+prompt = "Hello, how are you?"
+
+# # # Test the API
+print("Response from ChatGPT:", response)
 
 
-def check_example(definition):
-    example = definition.get("example", None)
-    if not example:
-        result = suggest_example({"word": definition["word"], "definition": definition["definition"]})
-        if result:
-            example = result["example"]
-    definition["example"]  = example
+# # from db import word_of_the_day
+# # from datetime import datetime
+
+# # current_date = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+
+# #     # Find document with date matching current date
+# # document = word_of_the_day.find_one({"date": current_date})
+
+# # print(str(datetime.now().date()))
+
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+
+# uri = os.getenv('DB_URI')
+# #uri = "mongodb+srv://soundsophisticated:soundsophisticated123@mercymercy.dyidanr.mongodb.net/?retryWrites=true&w=majority&appName=MercyMercy"
+# #uri = 'mongodb://soundsophisticated:soundsophisticated123@localhost:27017/soundsophisticated'
+
+
+# data = [
+#   {
+#     "word": "pronunciation",
+#     "phonetic": "/pɹəˌnaʊn.siˈeɪ.ʃən/",
+#     "phonetics": [
+#       {
+#         "text": "/pɹəˌnaʊn.siˈeɪ.ʃən/",
+#         "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/pronunciation-us.mp3",
+#         "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=589419",
+#         "license": {
+#           "name": "BY-SA 3.0",
+#           "url": "https://creativecommons.org/licenses/by-sa/3.0"
+#         }
+#       }
+#     ],
+#     "meanings": [
+#       {
+#         "partOfSpeech": "noun",
+#         "definitions": [
+#           {
+#             "definition": "The formal or informal way in which a word is made to sound when spoken.",
+#             "synonyms": [],
+#             "antonyms": [],
+#             "example": "What is the pronunciation of \"hiccough\"?"
+#           },
+#           {
+#             "definition": "The way in which the words of a language are made to sound when speaking.",
+#             "synonyms": [],
+#             "antonyms": [],
+#             "example": "His Italian pronunciation is terrible."
+#           },
+#           {
+#             "definition": "The act of pronouncing or uttering something.",
+#             "synonyms": [],
+#             "antonyms": []
+#           }
+#         ],
+#         "synonyms": [],
+#         "antonyms": []
+#       }
+#     ],
+#     "license": {
+#       "name": "CC BY-SA 3.0",
+#       "url": "https://creativecommons.org/licenses/by-sa/3.0"
+#     },
+#     "sourceUrls": [
+#       "https://en.wiktionary.org/wiki/pronunciation"
+#     ]
+#   }
+# ]
+
+# import requests
+# import json
+# from constants import SUGGEST_WORD_OF_THE_DAY_SYSTEM_ROLE, SUGGEST_EXAMPLE_SYSTEM_ROLE, SUGGEST_WORD_SYSTEM_ROLE, payload, headers
+
+# def suggest_example(word_and_definition):
+#         messages = [{"role": "system", "content": f"{SUGGEST_EXAMPLE_SYSTEM_ROLE}"}]
+#         messages.append({"role": "user", "content": f"{word_and_definition}"})
+#         payload["messages"] = messages
+
+#         try:
+#            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+#            if response.status_code == 200:
+#               return json.loads(response.json()["choices"][0]['message']['content'])
+#         except Exception as e:
+#            #we'll log these erros later
+#            print(e)
+
+
+# def extract_definitions(data):
+#     all_definitions = []
+#     for meaning in data["meanings"]:
+#         for definition in meaning["definitions"]:
+#             lean_definition = {
+#                 "word": data["word"], 
+#                 "part_of_speech": meaning["partOfSpeech"],
+#                 "definition": definition["definition"],
+#                 "example": definition.get("example", None)
+#             }
+#             all_definitions.append(lean_definition)
+
+#     return all_definitions
+
+
+# definitions = [
+#     {
+#         'word': 'pronunciation',
+#         'part_of_speech': 'noun',
+#         'definition': 'The formal or informal way in which a word is made to sound when spoken.',
+#         'example': 'What is the pronunciation of "hiccough"?'
+#     },
+#     {
+#         'word': 'pronunciation',
+#         'part_of_speech': 'noun',
+#         'definition': 'The way in which the words of a language are made to sound when speaking.',
+#         'example': None
+#     },
+#     {
+#         'word': 'pronunciation',
+#         'part_of_speech': 'noun',
+#         'definition': 'The act of pronouncing or uttering something.',
+#         'example': None
+#     }
+# ]
+
+
+# def check_example(definition):
+#     example = definition.get("example", None)
+#     if not example:
+#         result = suggest_example({"word": definition["word"], "definition": definition["definition"]})
+#         if result:
+#             example = result["example"]
+#     definition["example"]  = example
 
 
 
-def process_definitions(all_definitions):
-    # for definition in all_definitions:
-    #     check_example(definition)
+# def process_definitions(all_definitions):
+#     # for definition in all_definitions:
+#     #     check_example(definition)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(check_example, all_definitions)
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         executor.map(check_example, all_definitions)
         
-    print(all_definitions)
+#     print(all_definitions)
 
 
-import time
-import concurrent.futures
+# import time
+# import concurrent.futures
 
 
-if __name__ == "__main__":
-    start = time.time()
-    all_defs = extract_definitions(data[0])
-    processed = process_definitions(all_defs)
-    end = time.time()
+# if __name__ == "__main__":
+#     start = time.time()
+#     all_defs = extract_definitions(data[0])
+#     processed = process_definitions(all_defs)
+#     end = time.time()
 
-    print(f"Took {end-start} seconds")
+#     print(f"Took {end-start} seconds")
 #print(extract_definitions(data[0]))
